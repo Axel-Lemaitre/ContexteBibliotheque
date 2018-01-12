9170bd3189b69b5c53b390745f4a00d0d509afa7
Bobey <bobey.valentin@gmail.com>
2018-01-12 11:59:43 +0100

    avancement connexion users à corriger

--- app.js
+++ app.js
@@ -4,11 +4,54 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+let passport = require('passport');
+let Strategy = require('passport-local').Strategy;
+let session = require('express-session');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 var livre = require('./routes/livre');
 var android = require('./routes/android');
+
+let DAOUsers = require('./DAO/DAOUsers');
+let DAOusers = new DAOUsers();
+
+passport.use(new Strategy(
+    function(username, password, done) {
+        DAOusers.getUserByUsername(username ,
+            function(err, user) {
+                if (err) { return done(err); }
+                if (!user) {
+                    return done(null, false, { message: 'Incorrect username.' });
+                }
+
+                if (user.password !== password) {
+                    return done(null, false, { message: 'Incorrect password.' });
+                }
+                return done(null, user);
+            }
+        );
+    }
+));
+
+passport.serializeUser(
+    function(user, callback) {
+        callback(null, user.id);
+    }
+);
+passport.deserializeUser(
+    function(id, callback) {
+        DAOusers.getUserById(id,
+            function (err, user) {
+                if (err) {
+                    return callback(err);
+                }
+                callback(null, user);
+            }
+        );
+    }
+);
+
 
 var app = express();
 
@@ -23,6 +66,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', index);
 app.use('/users', users);
